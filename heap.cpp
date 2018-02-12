6b06d124600bfcffdc23b75f4c479faf9538d857
nickkpoon <nickkpoon@gmail.com>
2018-02-11 23:42:21 -0800

    error detection update

--- heap.cpp
+++ heap.cpp
@@ -5,14 +5,14 @@
 
 
 Heap::Heap(int32_t heap_size) : heap_size(heap_size), root_set() {
-  heap = new byte[heap_size];
-  from = heap;
-  to = heap + heap_size / 2;
-  bump_ptr = 0;
+    heap = new byte[heap_size];
+    from = heap;
+    to = heap + heap_size / 2;
+    bump_ptr = 0;
 }
 
 Heap::~Heap() {
-  delete[] heap;
+    delete[] heap;
 }
 
 // This method should print out the state of the heap.
@@ -20,7 +20,7 @@ Heap::~Heap() {
 // debugging tool. It's called whenever the DEBUG command is found
 // in the input program.
 void Heap::debug() {
-  // Implement me
+    // Implement me
 }
 
 // The allocate method allocates a chunk of memory of given size.
@@ -28,138 +28,155 @@ void Heap::debug() {
 // For example, the first allocated object would have the address 0.
 // IMPORTANT: This method should initiate garbage collection when there is not
 // enough memory. If there is still insufficient memory after garbage collection,
-// this method should throw an out_of_memory exception.
+// this method should throw an OutOfMemoryException.
 obj_ptr Heap::allocate(int32_t size) {
-  // Implement me
+    // Implement me
 }
 
 // This method should implement the actual semispace garbage collection.
 // As a final result this method *MUST* call print();
 void Heap::collect() {
-  // Implement me
-
-  // Please do not remove the call to print, it has to be the final
-  // operation in the method for your assignment to be graded.
-  print();
+    // Implement me
+    
+    // Please do not remove the call to print, it has to be the final
+    // operation in the method for your assignment to be graded.
+    print();
 }
 
 obj_ptr Heap::get_root(const std::string& name) {
-  auto root = root_set.find(name);
-  if(root == root_set.end()) {
-    throw std::runtime_error("No such root: " + name);
-  }
-
-  return root->second;
+    auto root = root_set.find(name);
+    if(root == root_set.end()) {
+        throw std::runtime_error("No such root: " + name);
+    }
+    
+    return root->second;
 }
 
 object_type Heap::get_object_type(obj_ptr ptr) {
-  return *reinterpret_cast<object_type*>(from + ptr);
+    return *reinterpret_cast<object_type*>(from + ptr);
 }
 
 // Finds fields by path / name; used by get() and set().
 obj_ptr *Heap::get_nested(const std::vector<std::string>& path) {
-  obj_ptr init = get_root(path[0]);
-  obj_ptr *fld = &init;
-
-  for(int i = 1; i < path.size(); ++i) {
-    auto addr = *fld;
-    auto type = *reinterpret_cast<object_type*>(global_address<object_type>(addr));
-    auto seg  = path[i];
-
-    switch(type) {
-    case FOO: {
-      auto *foo = global_address<Foo>(addr);
-      if(seg == "c") fld = &foo->c;
-      else if(seg == "d") fld = &foo->d;
-      else throw std::runtime_error("No such field: Foo." + seg);
-      break;
+    std::string current = path.at(0);
+    obj_ptr init = get_root(path[0]);
+    obj_ptr *fld = &init;
+    
+    for(size_t i = 1; i < path.size(); ++i) {
+        if(*fld == nil_ptr) {
+            std::string message("Nil pointer while getting: ");
+            throw std::runtime_error(message + current);
+        }
+        
+        auto addr = *fld;
+        auto type = get_object_type(addr);
+        auto seg  = path[i];
+        current  += "." + seg;
+        
+        switch(type) {
+            case FOO: {
+                auto *foo = global_address<Foo>(addr);
+                if(seg == "c") fld = &foo->c;
+                else if(seg == "d") fld = &foo->d;
+                else throw std::runtime_error("No such field: Foo." + seg);
+                break;
+            }
+            case BAR: {
+                auto *bar = global_address<Bar>(addr);
+                if(seg == "c") fld = &bar->c;
+                else if(seg == "f") fld = &bar->f;
+                else throw std::runtime_error("No such field: Bar." + seg);
+                break;
+            }
+            case BAZ: {
+                auto *baz = global_address<Baz>(addr);
+                if(seg == "b") fld = &baz->b;
+                else if(seg == "c") fld = &baz->c;
+                else throw std::runtime_error("No such field: Baz." + seg);
+                break;
+            }
+            default: {
+                std::string message("Unknown object type while getting: ");
+                throw std::runtime_error(message + std::to_string(int(type)));
+            }}
     }
-    case BAR: {
-      auto *bar = global_address<Bar>(addr);
-      if(seg == "c") fld = &bar->c;
-      else if(seg == "f") fld = &bar->f;
-      else throw std::runtime_error("No such field: Bar." + seg);
-      break;
-    }
-    case BAZ: {
-      auto *baz = global_address<Baz>(addr);
-      if(seg == "b") fld = &baz->b;
-      else if(seg == "c") fld = &baz->c;
-      else throw std::runtime_error("No such field: Baz." + seg);
-      break;
-    }}
-  }
-
-  return fld;
+    
+    return fld;
 }
 
 obj_ptr Heap::get(const std::vector<std::string>& path) {
-  if(path.size() == 1) {
-    return get_root(path[0]);
-  }
-  else {
-    return *get_nested(path);
-  }
+    if(path.size() == 1) {
+        return get_root(path[0]);
+    }
+    else {
+        return *get_nested(path);
+    }
 }
 
 void Heap::set(const std::vector<std::string>& path, obj_ptr value) {
-  if(path.size() == 1) {
-    if(value < 0) root_set.erase(path[0]);
-    else root_set[path[0]] = value;
-  }
-  else {
-    *get_nested(path) = value;
-  }
+    if(path.size() == 1) {
+        if(value < 0) root_set.erase(path[0]);
+        else root_set[path[0]] = value;
+    }
+    else {
+        *get_nested(path) = value;
+    }
 }
 
 obj_ptr Heap::new_foo() {
-  auto heap_addr = allocate(sizeof(Foo));
-  new (from + heap_addr) Foo(object_id++);
-  return heap_addr;
+    auto heap_addr = allocate(sizeof(Foo));
+    new (from + heap_addr) Foo(object_id++);
+    return heap_addr;
 }
 
 obj_ptr Heap::new_bar() {
-  auto heap_addr = allocate(sizeof(Bar));
-  new (from + heap_addr) Bar(object_id++);
-  return heap_addr;
+    auto heap_addr = allocate(sizeof(Bar));
+    new (from + heap_addr) Bar(object_id++);
+    return heap_addr;
 }
 
 obj_ptr Heap::new_baz() {
-  auto heap_addr = allocate(sizeof(Baz));
-  new (from + heap_addr) Baz(object_id++);
-  return heap_addr;
+    auto heap_addr = allocate(sizeof(Baz));
+    new (from + heap_addr) Baz(object_id++);
+    return heap_addr;
 }
 
 void Heap::print() {
-  byte *position = from;
-  std::map<int32_t, const char*> objects;
-
-  while(position < (from + heap_size / 2) && position < (from + bump_ptr)) {
-    object_type type = *reinterpret_cast<object_type*>(position);
-    switch(type) {
-      case FOO: {
-        auto obj = reinterpret_cast<Foo*>(position);
-        objects[obj->id] = "Foo";
-        position += sizeof(Foo);
-        break;
-      }
-      case BAR: {
-        auto obj = reinterpret_cast<Bar*>(position);
-        objects[obj->id] = "Bar";
-        position += sizeof(Bar);
-        break;
-      }
-      case BAZ: {
-        auto obj = reinterpret_cast<Baz*>(position);
-        objects[obj->id] = "Baz";
-        position += sizeof(Baz);
-        break;
-      }
+    obj_ptr position = 0;
+    typedef std::pair<obj_ptr,const char*> pair;
+    std::vector<pair> objects;
+    
+    while(position < heap_size / 2 && position < bump_ptr) {
+        object_type type = get_object_type(position);
+        switch(type) {
+            case FOO: {
+                auto obj = global_address<Foo>(position);
+                objects.push_back(pair(obj->id, "Foo"));
+                position += sizeof(Foo);
+                break;
+            }
+            case BAR: {
+                auto obj = global_address<Bar>(position);
+                objects.push_back(pair(obj->id, "Bar"));
+                position += sizeof(Bar);
+                break;
+            }
+            case BAZ: {
+                auto obj = global_address<Baz>(position);
+                objects.push_back(pair(obj->id, "Baz"));
+                position += sizeof(Baz);
+                break;
+            }
+            default: {
+                std::string message("Unknown object type while printing: ");
+                throw std::runtime_error(message + std::to_string(int(type)));
+            }
+        }
     }
-  }
-
-  std::cout << "Objects in from-space:\n";
-  for(auto const& itr: objects) {
-    std::cout << " - " << itr.first << ':' << itr.second << '\n';
-  }
+    
+    std::cout << "Objects in from-space:\n";
+    std::sort(objects.begin(), objects.end());
+    for(auto const& itr: objects) {
+        std::cout << " - " << itr.first << ':' << itr.second << '\n';
+    }
 }
--- main.cpp
+++ main.cpp
@@ -9,113 +9,111 @@
 
 #include "heap.hpp"
 
-// Removes spaces from a string (in place).
+// Removes spaces and comments from a string (in place).
 // Based on https://stackoverflow.com/a/83481
-void strip_whitespace(std::string& input) {
-  auto end = std::remove(input.begin(), input.end(), ' ');
-  input.erase(end, input.end());
+void clean(std::string& input) {
+    auto pos = input.find('#');
+    if(pos != std::string::npos) input.resize(pos);
+    auto end = std::remove(input.begin(), input.end(), ' ');
+    input.erase(end, input.end());
 }
 
 // Splits an assignment path (like a.b.c) into a vector of path segments.
 // Based on https://stackoverflow.com/a/14267455
 std::vector<std::string> split(const std::string& input) {
-  size_t start = 0;
-  std::vector<std::string> tokens;
-
-  while(true) {
-    size_t end = input.find('.', start);
-    if(end == std::string::npos) {
-      tokens.push_back(input.substr(start));
-      return tokens;
+    size_t start = 0;
+    std::vector<std::string> tokens;
+    
+    while(true) {
+        size_t end = input.find('.', start);
+        if(end == std::string::npos) {
+            tokens.push_back(input.substr(start));
+            return tokens;
+        }
+        else {
+            tokens.push_back(input.substr(start, end - start));
+            start = end + 1;
+        }
     }
-    else {
-      tokens.push_back(input.substr(start, end - start));
-      start = end + 1;
-    }
-  }
 }
 
 int main(int argc, char *argv[]) {
-  int size;
-
-  if(argc == 1) {
-    size = 200;
-  }
-  else if(argc == 2) {
-    size = std::atoi(argv[1]);
-    if(size < 1 || size % 2 != 0) {
-      std::cerr << "Heap size must be positive and even.\n";
-      std::cerr << "USAGE: ./a.out size=[200]\n";
-      exit(1);
+    int size;
+    
+    if(argc == 1) {
+        size = 200;
     }
-  }
-  else {
-    std::cerr << "USAGE: ./a.out size=[200]\n";
-    exit(1);
-  }
-
-  Heap heap(size);
-
-  while(true) {
+    else if(argc == 2) {
+        size = std::atoi(argv[1]);
+        if(size < 1 || size % 2 != 0) {
+            std::cerr << "Heap size must be positive and even.\n";
+            std::cerr << "USAGE: ./a.out size=[200]\n";
+            exit(1);
+        }
+    }
+    else {
+        std::cerr << "USAGE: ./a.out size=[200]\n";
+        exit(1);
+    }
+    
+    Heap heap(size);
     std::string line;
-    std::getline(std::cin, line);
-    if(std::cin.eof()) return 0;
-
-    strip_whitespace(line);
-    if(line.length() == 0 || line[0] == '#') {
-      // Blank or a comment; skip it.
-      continue;
+    while(std::getline(std::cin, line)) {
+        clean(line);
+        if(line.length() == 0) {
+            // These are not the lines you're looking for.
+            continue;
+        }
+        
+        if(line == "PRINT") {
+            heap.print();
+            continue;
+        }
+        if(line == "DEBUG") {
+            heap.debug();
+            continue;
+        }
+        if(line == "COLLECT") {
+            heap.collect();
+            continue;
+        }
+        
+        auto pos = line.find('=');
+        if(pos == std::string::npos) {
+            std::cerr << "Bad command: " << line << '\n';
+            continue;
+        }
+        
+        auto lhs = line.substr(0, pos);
+        auto rhs = line.substr(pos + 1);
+        auto obj = obj_ptr(-1);
+        
+        try {
+            if(rhs == "Foo") {
+                obj = heap.new_foo();
+                // std::cout << "Allocated Foo " << lhs << " at " << obj << std::endl;
+            }
+            else if(rhs == "Bar") {
+                obj = heap.new_bar();
+                // std::cout << "Allocated Bar " << lhs << " at " << obj << std::endl;
+            }
+            else if(rhs == "Baz") {
+                obj = heap.new_baz();
+                // std::cout << "Allocated Baz " << lhs << " at " << obj << std::endl;
+            }
+            else if(rhs == "NULL") {
+                obj = obj_ptr(-1);
+            }
+            else {
+                auto rpath = split(rhs);
+                obj = heap.get(rpath);
+            }
+            
+            auto lpath = split(lhs);
+            heap.set(lpath, obj);
+        }
+        catch(OutOfMemoryException e) {
+            std::cout << "OUT OF MEMORY\n";
+        }
     }
-
-    if(line == "PRINT") {
-      heap.print();
-      continue;
-    }
-    if(line == "DEBUG") {
-      heap.debug();
-      continue;
-    }
-    if(line == "COLLECT") {
-      heap.collect();
-      continue;
-    }
-
-    int pos = line.find('=');
-    if(pos == std::string::npos) {
-      std::cerr << "Bad command: " << line << '\n';
-      continue;
-    }
-
-    auto lhs = line.substr(0, pos);
-    auto rhs = line.substr(pos + 1);
-    auto obj = obj_ptr(-1);
-
-    try {
-      if(rhs == "Foo") {
-        obj = heap.new_foo();
-        // std::cout << "Allocated Foo " << lhs << " at " << obj << std::endl;
-      }
-      else if(rhs == "Bar") {
-        obj = heap.new_bar();
-        // std::cout << "Allocated Bar " << lhs << " at " << obj << std::endl;
-      }
-      else if(rhs == "Baz") {
-        obj = heap.new_baz();
-        // std::cout << "Allocated Baz " << lhs << " at " << obj << std::endl;
-      }
-      else if(rhs == "NULL") {
-        obj = obj_ptr(-1);
-      }
-      else {
-        auto rpath = split(rhs);
-        obj = heap.get(rpath);
-      }
-
-      auto lpath = split(lhs);
-      heap.set(lpath, obj);
-    }
-    catch(OutOfMemoryException e) {
-      std::cout << "OUT OF MEMORY\n";
-    }
-  }
 }
