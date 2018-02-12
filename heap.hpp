819a882697dcaaaa6e07cd6c5e6b6ee7ca1efee0
nickkpoon <nickkpoon@gmail.com>
2018-02-12 01:25:12 -0800

    corrected heap.hpp, previous commit not functional at all

--- heap.hpp
+++ heap.hpp
@@ -1,182 +1,190 @@
-#include "heap.hpp"
-
-#include <iostream>
+#include <cstdint>
+#include <stdexcept>
+#include <string>
 #include <map>
+#include <vector>
 
+typedef int8_t byte;
+typedef int32_t obj_ptr;
 
-Heap::Heap(int32_t heap_size) : heap_size(heap_size), root_set() {
-    heap = new byte[heap_size];
-    from = heap;
-    to = heap + heap_size / 2;
-    bump_ptr = 0;
-}
+// All addresses on the heap are relative to the `from` pointer.
+// For example, allocating an object on an empty heap will give it
+// an address 0. Because of this, we use a special constant nil_ptr
+// to indicate nil pointers.
+//
+static const obj_ptr nil_ptr(-1);
 
-Heap::~Heap() {
-    delete[] heap;
-}
+// The enum below will always be the first member of all structs
+// that can be allocated on the heap. It tags the object with its
+// type, making it possible to easily cast a byte pointer to a
+// pointer to an appropriate struct. This is also useful
+// when calculating struct sizes and offsets.
+// The object type tag will always be a single byte in size.
+//
+enum object_type : byte {
+    // These are set to values you're unlikely to find in uninitialized
+    // memory so you have a better shot at detecting errors:
+    FOO = 42,
+    BAR = 13,
+    BAZ = 34
+};
 
-// This method should print out the state of the heap.
-// It's not mandatory to implement it, but would be a very useful
-// debugging tool. It's called whenever the DEBUG command is found
-// in the input program.
-void Heap::debug() {
-    // Implement me
-}
+// There are three possible objects we can allocate on the heap,
+// Foo, Bar and Baz. Each contain the following:
+// - type (an object_type tag)
+// - id a unique object id, used to print all currently live objects
+//   and their types
+// - pointers to other objects, of type obj_ptr
+// - non-pointer data, which will always be 0 and is of type `byte`
+//
+// So for example, the struct Foo has c and d as its pointer members,
+// and a and b as data members. You *do not* have to do anything with
+// the data members, they only exist to model a realistic looking struct.
 
-// The allocate method allocates a chunk of memory of given size.
-// It returns an object pointer, which is local to the from space.
-// For example, the first allocated object would have the address 0.
-// IMPORTANT: This method should initiate garbage collection when there is not
-// enough memory. If there is still insufficient memory after garbage collection,
-// this method should throw an OutOfMemoryException.
-obj_ptr Heap::allocate(int32_t size) {
-    // Implement me
-}
-
-// This method should implement the actual semispace garbage collection.
-// As a final result this method *MUST* call print();
-void Heap::collect() {
-    // Implement me
+struct Foo {
+    object_type type = FOO;
+    uint32_t   id;
     
-    // Please do not remove the call to print, it has to be the final
-    // operation in the method for your assignment to be graded.
-    print();
-}
+    byte       a = 0;
+    byte       b = 0;
+    obj_ptr    c = nil_ptr;
+    obj_ptr    d = nil_ptr;
+    
+    Foo(uint32_t id) : id(id) {}
+};
 
-obj_ptr Heap::get_root(const std::string& name) {
-    auto root = root_set.find(name);
-    if(root == root_set.end()) {
-        throw std::runtime_error("No such root: " + name);
+struct Bar {
+    object_type type = BAR;
+    uint32_t   id;
+    
+    byte       a = 0;
+    byte       b = 0;
+    obj_ptr    c = nil_ptr;
+    byte       d = 0;
+    byte       e = 0;
+    obj_ptr    f = nil_ptr;
+    
+    Bar(uint32_t id) : id(id) {}
+};
+
+struct Baz {
+    object_type type = BAZ;
+    uint32_t   id;
+    
+    byte       a = 0;
+    obj_ptr    b = nil_ptr;
+    obj_ptr    c = nil_ptr;
+    
+    Baz(uint32_t id) : id(id) {}
+};
+
+// This exception should be thrown when an allocation is attempted, but
+// garbage collection is unable to free the needed space.
+// For example, if we try to allocate 20 bytes, but have only 10 free
+// even after running garbage collection, the implementation should
+// throw this exception.
+//
+struct OutOfMemoryException : public std::runtime_error {
+    OutOfMemoryException() : std::runtime_error("Out of memory") {}
+};
+
+// This is your heap class, it contains everything you need to implement
+// garbage collection. You should only implement the following methods:
+// - allocate(int32_t size)
+// - collect()
+// Optionally, you might want to implement dump_heap for debugging purposes.
+// This method should be used to dump the state of the heap and help you
+// debug problems in your implementation.
+class Heap {
+    // heap is the pointer to the actual memory buffer we allocate objects in
+    byte *heap;
+    // from points to the beginning of the from-space
+    byte *from;
+    // to points to the beginning of the to-space
+    byte *to;
+    // heap_size is the size of the whole heap, and will always be an even number
+    int32_t heap_size;
+    // bump_ptr is the bump pointer in the from space, relative to the from pointer.
+    // This means that it will initially be 0, and is incremented as allocation happens.
+    // If we were to allocate 10 bytes on an empty heap, bump_ptr would then point to 10.
+    int32_t bump_ptr;
+    
+    // This is your root set, it maps variables to object pointers (again, object pointers
+    // are relative to the from pointer). If you were to allocate objects of sizes 5 and 10
+    // on an empty heap, and assign them to x and y, your root set would look like this:
+    // x -> 0
+    // y -> 5
+    std::map<std::string, obj_ptr> root_set;
+    // This is the object id counter, it assigns every new object a unique id.
+    // You don't need to touch this in any way.
+    uint32_t object_id = 0;
+    
+public:
+    Heap(int32_t heap_size);
+    ~Heap();
+    
+    // You should define allocate in impl.cpp
+    obj_ptr allocate(int32_t size);
+    
+    // You should define collect in impl.cpp
+    void    collect();
+    
+    // You should optionally define debug in impl.cpp
+    void    debug();
+    
+    // This method will dump (print) all objects currently in the from space.
+    // Do *not* alter this, as it is used for grading. It can also help you
+    // debug your implementation.
+    void    print();
+    
+    
+    // The following three methods create instances of Foo, Bar and Baz respectively.
+    // In the background, they will call your allocate() method, and will properly
+    // initialize the memory. You only need to worry about memory allocation.
+    obj_ptr new_foo();
+    obj_ptr new_bar();
+    obj_ptr new_baz();
+    
+    // get(path) returns a pointer to the object by following a path from the root
+    // set. For example, if you have:
+    // x = Foo
+    // x.c = Bar
+    // x.c.f = Baz
+    // then calling get("x.c.f") will return the address of the Baz object.
+    // If the object can not be found or the path is invalid, an exception is thrown.
+    obj_ptr  get(const std::vector<std::string>& path);
+    
+    // set(path, obj_ptr) will assign an object pointer to a path.
+    // In the previous example, doing set("x.c.f", nil_ptr) will set
+    // the Bar object's pointer 'f' to nil.
+    void     set(const std::vector<std::string>& path, obj_ptr value);
+    
+    // The following two methods are auxiliary, you should use get() instead.
+    obj_ptr* get_nested(const std::vector<std::string>& path);
+    obj_ptr  get_root(const std::string& name);
+    
+    // This is a convenience method that, given a pointer of an object that resides
+    // in the from space, will return its type (ie. FOO, BAR or BAZ).
+    object_type get_object_type(obj_ptr);
+    
+    // Converts a global pointer (to an actual struct) to a heap-local pointer.
+    // For example, if you have an object Foo at the beginning of the heap,
+    // and would like its local address (which in this case would be 0), you can do:
+    // obj_ptr p = local_address(heap)
+    //
+    template<class T>
+    int32_t local_address(T *obj) {
+        return (int32_t)(reinterpret_cast<byte*>(obj) - from);
     }
     
-    return root->second;
-}
-
-object_type Heap::get_object_type(obj_ptr ptr) {
-    return *reinterpret_cast<object_type*>(from + ptr);
-}
-
-// Finds fields by path / name; used by get() and set().
-obj_ptr *Heap::get_nested(const std::vector<std::string>& path) {
-    std::string current = path.at(0);
-    obj_ptr init = get_root(path[0]);
-    obj_ptr *fld = &init;
-    
-    for(size_t i = 1; i < path.size(); ++i) {
-        if(*fld == nil_ptr) {
-            std::string message("Nil pointer while getting: ");
-            throw std::runtime_error(message + current);
-        }
-        
-        auto addr = *fld;
-        auto type = get_object_type(addr);
-        auto seg  = path[i];
-        current  += "." + seg;
-        
-        switch(type) {
-            case FOO: {
-                auto *foo = global_address<Foo>(addr);
-                if(seg == "c") fld = &foo->c;
-                else if(seg == "d") fld = &foo->d;
-                else throw std::runtime_error("No such field: Foo." + seg);
-                break;
-            }
-            case BAR: {
-                auto *bar = global_address<Bar>(addr);
-                if(seg == "c") fld = &bar->c;
-                else if(seg == "f") fld = &bar->f;
-                else throw std::runtime_error("No such field: Bar." + seg);
-                break;
-            }
-            case BAZ: {
-                auto *baz = global_address<Baz>(addr);
-                if(seg == "b") fld = &baz->b;
-                else if(seg == "c") fld = &baz->c;
-                else throw std::runtime_error("No such field: Baz." + seg);
-                break;
-            }
-            default: {
-                std::string message("Unknown object type while getting: ");
-                throw std::runtime_error(message + std::to_string(int(type)));
-            }}
+    // This function converts a local address (an obj_ptr) to an actual
+    // pointer to the struct on the heap. If you have an object at address 12
+    // in the heap, and you know it to be of type Foo, then doing:
+    //   global_address<Foo>(12)
+    // will return a Foo* pointer, that can be used to read or modify the
+    // actual structure.
+    template<class T>
+    T* global_address(int32_t addr) {
+        return reinterpret_cast<T*>(from + addr);
     }
-    
-    return fld;
-}
-
-obj_ptr Heap::get(const std::vector<std::string>& path) {
-    if(path.size() == 1) {
-        return get_root(path[0]);
-    }
-    else {
-        return *get_nested(path);
-    }
-}
-
-void Heap::set(const std::vector<std::string>& path, obj_ptr value) {
-    if(path.size() == 1) {
-        if(value < 0) root_set.erase(path[0]);
-        else root_set[path[0]] = value;
-    }
-    else {
-        *get_nested(path) = value;
-    }
-}
-
-obj_ptr Heap::new_foo() {
-    auto heap_addr = allocate(sizeof(Foo));
-    new (from + heap_addr) Foo(object_id++);
-    return heap_addr;
-}
-
-obj_ptr Heap::new_bar() {
-    auto heap_addr = allocate(sizeof(Bar));
-    new (from + heap_addr) Bar(object_id++);
-    return heap_addr;
-}
-
-obj_ptr Heap::new_baz() {
-    auto heap_addr = allocate(sizeof(Baz));
-    new (from + heap_addr) Baz(object_id++);
-    return heap_addr;
-}
-
-void Heap::print() {
-    obj_ptr position = 0;
-    typedef std::pair<obj_ptr,const char*> pair;
-    std::vector<pair> objects;
-    
-    while(position < heap_size / 2 && position < bump_ptr) {
-        object_type type = get_object_type(position);
-        switch(type) {
-            case FOO: {
-                auto obj = global_address<Foo>(position);
-                objects.push_back(pair(obj->id, "Foo"));
-                position += sizeof(Foo);
-                break;
-            }
-            case BAR: {
-                auto obj = global_address<Bar>(position);
-                objects.push_back(pair(obj->id, "Bar"));
-                position += sizeof(Bar);
-                break;
-            }
-            case BAZ: {
-                auto obj = global_address<Baz>(position);
-                objects.push_back(pair(obj->id, "Baz"));
-                position += sizeof(Baz);
-                break;
-            }
-            default: {
-                std::string message("Unknown object type while printing: ");
-                throw std::runtime_error(message + std::to_string(int(type)));
-            }
-        }
-    }
-    
-    std::cout << "Objects in from-space:\n";
-    std::sort(objects.begin(), objects.end());
-    for(auto const& itr: objects) {
-        std::cout << " - " << itr.first << ':' << itr.second << '\n';
-    }
-}
+};
